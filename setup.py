83a801718b265476f6e42291a80f27b09ea61392
Shawn Smith <shawnpsmith@gmail.com>
2012-08-21 11:45:23 -0700

    add pep8 cmdclass to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,37 @@
 from setuptools import setup
 
+
+class Pep8Command(Command):
+    description = "run pep8 script"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            import pep8
+            pep8
+        except ImportError:
+            print ('Missing "pep8" library. You can install it using pip: '
+                  'pip install pep8')
+            sys.exit(1)
+
+        cwd = os.getcwd()
+        retcode = call(('pep8 %s/libcloud/' %
+                (cwd)).split(' '))
+        sys.exit(retcode)
+
+
 setup(
     name='txKeystone',
     version='0.1',
-    description='A Twisted Agent implementation which authenticates to Keystone and uses the keystone auth credentials to authenticate against requested urls.',
+    description='A Twisted Agent implementation which authenticates' +
+                ' to Keystone and uses the Keystone auth credentials' +
+                ' to authenticate against requested urls.',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
@@ -12,6 +40,9 @@ setup(
     ],
     license='APL2',
     url='https://github.com/racker/python-twisted-keystone-agent',
+    cmdclass={
+        'pep8': Pep8Command
+    },
     packages=['txKeystone'],
     install_requires=['Twisted'],
 )
