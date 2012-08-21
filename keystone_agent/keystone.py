aa5523692e3f878c657376f7bf0951fd9d526a05
Shawn Smith <shawnpsmith@gmail.com>
2012-08-20 17:33:29 -0700

    many style fixups

--- keystone_agent/keystone.py
+++ keystone_agent/keystone.py
@@ -1,3 +1,4 @@
+import httplib
 import json
 
 from cStringIO import StringIO
@@ -13,11 +14,14 @@ from twisted.python import log
 
 class KeystoneAgent(object):
     """
-    Fetches and inserts X-Auth-Token and X-Tenant-Id headers into requests made using this agent.
+    Fetches and inserts X-Auth-Token and X-Tenant-Id headers into requests
+    made using this agent.
 
-    @cvar auth_headers: Dictionary in the form ("X-Tenant-Id": "id", "X-Auth-Token": "token")
-                        containing current authentication header data.
-    @cvar MAX_RETRIES:  Maximum number of connection attempts to make before failing.
+    @cvar auth_headers: Dictionary in the form
+    ("X-Tenant-Id": "id", "X-Auth-Token": "token") containing current
+    authentication header data.
+    @cvar MAX_RETRIES:  Maximum number of connection attempts to make
+    before failing.
     """
     MAX_RETRIES = 3
 
@@ -47,7 +51,10 @@ class KeystoneAgent(object):
     def request(self, method, uri, headers=None, bodyProducer=None):
         self.msg("request (%(method)s): %(uri)s", method=method, uri=uri)
 
-        return self._request(method, uri, headers=headers, bodyProducer=bodyProducer)
+        return self._request(method,
+                             uri,
+                             headers=headers,
+                             bodyProducer=bodyProducer)
 
     def _request(self, method, uri, headers=None, bodyProducer=None, depth=0):
         self.msg("_request depth %(depth)s (%(method)s): %(uri)s",
@@ -57,61 +64,79 @@ class KeystoneAgent(object):
             headers = Headers()
 
         if depth == self.MAX_RETRIES:
-            return fail(AuthenticationError("Authentication headers rejected after max retries"))
+            return fail(AuthenticationError("Authentication headers"
+                                            "rejected after max retries"))
 
         def _handleResponse(response, method=method, uri=uri, headers=headers):
             self.msg("_handleResponse (%(method)s): %(uri)s",
                      method=method, uri=uri, depth=depth)
 
             if response.code == 401:
-                #The auth headers were not accepted, force an update and recurse
-                self.auth_headers = {"X-Auth-Token": None, "X-Tenant-Id": None}
+                #The auth headers were not accepted, force an update and
+                # recurse
+                self.auth_headers = \
+                    {"X-Auth-Token": None, "X-Tenant-Id": None}
                 self._state = self.NOT_AUTHENTICATED
 
-                return self._request(method, uri, headers=headers, bodyProducer=bodyProducer, depth=depth+1)
+                return self._request(method,
+                                     uri,
+                                     headers=headers,
+                                     bodyProducer=bodyProducer,
+                                     depth=depth + 1)
             else:
                 #The auth headers were accepted, return the response
                 return response
 
         def _makeRequest(auth_headers):
-            self.msg("_makeRequest  %(auth_headers)s (%(method)s): %(uri)s",
-                     method=method, uri=uri, auth_headers=auth_headers)
+            self.msg("_makeRequest %(auth_headers)s (%(method)s): %(uri)s",
+                     auth_headers=auth_header, smethod=method, uri=uri)
 
             for header, value in auth_headers.items():
                 headers.setRawHeaders(header, [value])
 
-            req = self.agent.request(method, uri, headers=headers, bodyProducer=bodyProducer)
+            req = self.agent.request(method,
+                                     uri,
+                                     headers=headers,
+                                     bodyProducer=bodyProducer)
             req.addCallback(_handleResponse)
             return req
 
-        #Asynchronously get the auth headers, then make the request using them
+        #Asynchronously get the auth headers, then make the request using
+        # them
         d = self._getAuthHeaders()
         d.addCallback(_makeRequest)
         return d
 
     def _getAuthRequestBodyProducer(self):
-        return FileBodyProducer(StringIO(json.dumps({"auth":
-                                                     {"passwordCredentials":
-                                                      {"username": self.auth_cred[0],
-                                                       "password": self.auth_cred[1]}}})))
+        auth_dict = {"auth":
+                        {"passwordCredentials":
+                            {"username": self.auth_cred[0],
+                             "password": self.auth_cred[1]}}}
+
+        return FileBodyProducer(StringIO(json.dumps(auth_dict)))
 
     def _getAuthHeaders(self):
         """
-        Get authentication headers. If we have valid header data already, they immediately return it.
-        If not, then get new authentication data.  If we are currently in the process of getting the
-        header data, put this request into a queue to be handled when the data are received.
+        Get authentication headers. If we have valid header data already,
+        they immediately return it.
+        If not, then get new authentication data.  If we are currently in
+        the process of getting the
+        header data, put this request into a queue to be handled when the
+        data are received.
 
-        @returns:   A deferred that will eventually be called back with the header data
+        @returns: A deferred that will eventually be called back with the
+        header data
         """
         def _handleAuthBody(body):
             self.msg("_handleAuthBody: %(body)s", body=body)
 
             try:
                 body_parsed = json.loads(body)
+                access_token = body_parsed['access']['token']
 
-                tenant_id = body_parsed['access']['token']['tenant']['id'].encode('ascii')
-                auth_token = body_parsed['access']['token']['id'].encode('ascii')
-                auth_token_expires = body_parsed['access']['token']['expires'].encode('ascii')
+                tenant_id = access_token['tenant']['id'].encode('ascii')
+                auth_token = access_token['id'].encode('ascii')
+                auth_token_expires = access_token['expires'].encode('ascii')
 
                 self.auth_headers["X-Tenant-Id"] = tenant_id
                 self.auth_headers["X-Auth-Token"] = auth_token
@@ -119,8 +144,10 @@ class KeystoneAgent(object):
 
                 self._state = self.AUTHENTICATED
 
-                self.msg("_handleAuthHeaders: found token %(token)s, tenant id %(tenant_id)s",
-                         token=self.auth_headers["X-Auth-Token"], tenant_id=self.auth_headers["X-Tenant-Id"])
+                self.msg("_handleAuthHeaders: found token %(token)s"
+                         " tenant id %(tenant_id)s",
+                         token=self.auth_headers["X-Auth-Token"],
+                         tenant_id=self.auth_headers["X-Tenant-Id"])
 
                 # Callback all queued auth headers requests
                 while not self._headers_requests.empty():
@@ -128,41 +155,52 @@ class KeystoneAgent(object):
 
             except ValueError as e:
                 # We received a bad response
-                return fail(MalformedJSONError("Malformed keystone response received"))
+                return fail(MalformedJSONError("Malformed keystone"
+                                               "response received"))
 
         def _handleAuthResponse(response):
             if response.code == 200:
-                self.msg("_handleAuthResponse: %(response)s accepted", response=response)
+                self.msg("_handleAuthResponse: %(response)s accepted",
+                         response=response)
                 body = Deferred()
                 response.deliverBody(StringIOReceiver(body))
                 body.addCallback(_handleAuthBody)
                 return body
             else:
-                self.msg("_handleAuthResponse: %(response)s rejected", response=response)
-                return fail(KeystoneAuthenticationError("Keystone authentication credentials rejected"))
+                self.msg("_handleAuthResponse: %(response)s rejected",
+                         response=response)
+                return fail(KeystoneAuthenticationError("Keystone"
+                    " authentication credentials rejected"))
 
         self.msg("_getAuthHeaders: state is %(state)s", state=self._state)
 
         if self._state == self.AUTHENTICATED:
-            # We are authenticated, immediately succeed with the current auth headers
-            self.msg("_getAuthHeaders: succeed with %(headers)s", headers=self.auth_headers)
+            # We are authenticated, immediately succeed with the current
+            # auth headers
+            self.msg("_getAuthHeaders: succeed with %(headers)s",
+                     headers=self.auth_headers)
 
             return succeed(self.auth_headers)
-        elif self._state == self.NOT_AUTHENTICATED or self._state == self.AUTHENTICATING:
-            # We cannot satisfy the auth header request immediately, put it in a queue
+        elif self._state == self.NOT_AUTHENTICATED or \
+             self._state == self.AUTHENTICATING:
+            # We cannot satisfy the auth header request immediately, put it
+            # in a queue
             self.msg("_getAuthHeaders: defer, place in queue")
             auth_headers_deferred = Deferred()
             self._headers_requests.put(auth_headers_deferred)
 
             if self._state == self.NOT_AUTHENTICATED:
-                self.msg("_getAuthHeaders: not authenticated, start authentication process")
-                # We are not authenticated, and not in the process of authenticating.
-                # Set our state to authenticating and begin the authentication process
+                self.msg("_getAuthHeaders: not authenticated, start"
+                         " authentication process")
+                # We are not authenticated, and not in the process of
+                # authenticating.
+                # Set our state to authenticating and begin the
+                # authentication process
                 self._state = self.AUTHENTICATING
 
                 d = self.agent.request('POST', self.auth_url,
-                                       Headers({"Content-type": ["application/json"]}),
-                                       self._getAuthRequestBodyProducer())
+                        Headers({"Content-type": ["application/json"]}),
+                        self._getAuthRequestBodyProducer())
                 d.addCallback(_handleAuthResponse)
 
             return auth_headers_deferred
@@ -186,13 +224,13 @@ class MalformedJSONError(Exception):
 
 class StringIOReceiver(Protocol):
     """
-    A protocol to aggregate chunked data as its received, and fire a callback
-    with the aggregated data when the connection is closed.
+    A protocol to aggregate chunked data as its received, and fire a
+    callback with the aggregated data when the connection is closed.
     """
 
     def __init__(self, finished):
         """
-        @param body:   Deferred to fire when all data have been aggregated.
+        @param body: Deferred to fire when all data have been aggregated.
         """
         self.buffer = StringIO()
         self.finished = finished
